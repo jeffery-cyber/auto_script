af0ac34fa24c29c28c1d272674cfc5de7fcbb632
jeffery-cyber <61228035+jeffery-cyber@users.noreply.github.com>
2021-04-06 14:41:03 +0800

    Update bububaoCOOKIE.js

--- BBB_Task/bububaoCOOKIE.js
+++ BBB_Task/bububaoCOOKIE.js
@@ -1,8 +1,6 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let bububaotokenVal = `43F7876A4F29C53D5B3CA9D5D530283G1612665750
-                       1AF1971906164B677FF8A2B96530348G1612663771
-                       95230100FE2A362A15B4BF4E8530320G1612665258`
+let bububaotokenVal = `ot2qag2u581pp2soos1sg554o2`
 
 
 
